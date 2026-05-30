580d7e99ea83a498d80346c94e79caf5d016863f
Rupesh883 <rupeshbk650@gmail.com>
2026-05-30 17:46:18 +0545

    project complted

---
+++ Assignments/assignment6/project1/index.js
@@ -0,0 +1,9 @@
+
+  const nav = document.querySelector("nav");
+  window.addEventListener("scroll" , () => {
+    if (window.scrollY > 50) {
+      nav.classList.add("active-1");
+    } else {
+      nav.classList.remove("active-1");
+    }
+  });
